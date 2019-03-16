53c6b4971f16fd512e24d1ae15caada225e8908a
unknown <mrgaozhihong@gmail.com>
2019-03-16 22:48:14 +0800

    add loadcsv

--- util/doc_util.py
+++ util/doc_util.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import pandas as pd
 
 path=os.getcwd()
 
@@ -20,5 +21,12 @@ def savecsv(data,name):
     file_path=path+'\\save\\'+name+'.csv'
     data.to_csv(file_path,sep=',',index=False)
     print('save to {}'.format(file_path))
+
+def loadcsv(name):
+    global path
+    file_path=path+'\\save\\'+name+'.csv'
+    print('load {}'.format(file_path))
+    data=pd.read_csv(file_path)
+    return data
 
 
