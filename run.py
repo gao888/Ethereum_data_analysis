598aad8011ebbcfb63c8110fa577943314092818
unknown <mrgaozhihong@gmail.com>
2019-03-16 22:47:51 +0800

    rename file

--- run.py
+++ run.py
@@ -1,21 +1,18 @@
 from util import doc_util as doc
-from util import ethscrap_util as es
+from util import spider_util as sp
 
 token_name='SeeleToken'
 token_address='0xb1eef147028e9f480dbc5ccaa3277d417d1b85f0'
 
 #get all transactions of token
-table=es.main(token_address)
+table=sp.main(token_address)
 
 #save all transactions
 filename=token_name+'_tx'
 doc.savecsv(table,filename)
-#adjust format of transation data
-# totaltx=doc.adjust_txdata(totaltx,token_address)
 
-#save transation data
-# doc.savecsv(totaltx,token_name)
-
+#load all transactions
+totaltx=doc.loadcsv(filename)
 
 #get all hashs of token
 # hashs=es.get_hash_all(token_address)
